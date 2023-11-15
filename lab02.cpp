c4a96670054f0211223c6b38f7b1f16c59e22900
Arthur Cabral Fleischman <33187588+ArthurFleischman@users.noreply.github.com>
2023-11-15 23:03:20 +0100

    fixing activaiton of task and blinking

--- lab02.cpp
+++ lab02.cpp
@@ -3,6 +3,7 @@
 #include "tpl_os.h"
 #include "Arduino.h"
 #include "math.h"
+#define BTN_TIME 10
 enum LED_MSG{ALL_OFF, BLINK_FAST, BLINK_SLOW, ALL_ON};
 
 int concatNumbers(int num1, int num2) {
@@ -11,30 +12,34 @@ int concatNumbers(int num1, int num2) {
 }
 int decodeDigitalState(int code) {
     /*
-     shifts right all the number until only the digital state remains
-     */
+       shifts right all the number until only the digital state remains
+       */
     return (code >> 11);
 }
 int decodeAnalogState(int code){
     /*
-        use the inverted state of the eleventh bit in a bitwise and operation
-        to eliminate the value of digital signal from the code
-    */
+       use the inverted state of the eleventh bit in a bitwise and operation
+       to eliminate the value of digital signal from the code
+       */
     return code &= (~(decodeDigitalState(code)) << 11);
 }
 
 void setup()
 {
-	pinMode(13, OUTPUT);
-    pinMode(12, INPUT);
+    Serial.begin(9600);
+    Serial.print("ok\n");
+    pinMode(13, OUTPUT);
+    pinMode(12, INPUT_PULLUP);
 }
 
 TASK(Task_M)
 {
+
     int msg = ALL_ON;
     static int analogRefValue= 0;
     static bool has_ref = false;
     if(ReceiveMessage(M_receive, &msg) == E_OK){
+        Serial.print("message received\n");
         int analogValue = decodeAnalogState(msg);
         int digitalValue = decodeDigitalState(msg);
         if(digitalValue){
@@ -55,52 +60,59 @@ TASK(Task_M)
         }
         SendMessage(msgfrom_M, &msg);
     }
+    TerminateTask();
 }
 TASK(Task_V)
 {
-    
+
     static int counter = 0;
     static bool led_state = false;
-    int msg = 3;
-    if(ReceiveMessage(V_receive, &msg) == E_OK){
-        switch(msg){
-            case 0:
-                digitalWrite(13, LOW);
-                break;
+    int msg = BLINK_SLOW;
+    ReceiveMessage(V_receive, &msg);
+    switch(msg){
+        case ALL_OFF:
+            digitalWrite(13, LOW);
+            break;
             // 1 state change per second, counter == 4 -> led change state`
-            case 1:
-                if(counter >= 4){
-                    led_state = !led_state;
-                    digitalWrite(13, led_state);
-                    counter = 0;
-                }
-                break;
+        case BLINK_FAST:
+            led_state = !led_state;
+            digitalWrite(13, led_state);
+            break;
             // 1 state change every 1/4 seconds, counter == 1 -> led change state;
-            case 2:
+        case BLINK_SLOW:
+            if(counter >= 4){
                 led_state = !led_state;
                 digitalWrite(13, led_state);
-                break;
-            case 3:
-                digitalWrite(13, HIGH);
-                break;
-        }
+                counter = 0;
+            }
+            break;
+        case ALL_ON:
+            digitalWrite(13, HIGH);
+            break;
     }
-	TerminateTask();
+
+    Serial.print("counter from V: ");
+    Serial.println(counter);
+    counter++;
+    TerminateTask();
 }
 
 TASK(Periodic_Task_C)
 {
     static int btn_counter = 0;
     int concatResult = 0;
-	int analogValue = analogRead(0);
-    int digitalValue = digitalRead(12);
+    int analogValue = analogRead(0);
+    int digitalValue = !digitalRead(12);
     if(digitalValue){
         btn_counter++;
-        concatResult = concatNumbers(0, analogValue);
+        if(btn_counter >= BTN_TIME){
+            concatResult = concatNumbers(digitalValue, analogValue);
+            btn_counter = 0;
+        }
     }
-    if(btn_counter >= 10){
-        concatResult = concatNumbers(digitalValue, analogValue);
+    else{
         btn_counter = 0;
+        concatResult = concatNumbers(0, analogValue);
     }
     SendMessage(msgfrom_C, &concatResult);
     TerminateTask();
